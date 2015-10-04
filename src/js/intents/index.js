344ce570fbe53168e51ca35030168272c91fe40a
fand <fand@gmork.in>
2015-10-04 21:16:01 +0900

    Cast bpm input values to number

--- src/js/intents/index.js
+++ src/js/intents/index.js
@@ -1,5 +1,5 @@
 export default function intent (DOM) {
   return {
-    changeBPM$ : DOM.select('#bpm').events('input').map(ev => ev.target.value).startWith(144),
+    changeBPM$ : DOM.select('#bpm').events('input').map(ev => +ev.target.value).startWith(144),
   };
 }
