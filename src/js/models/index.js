864437b726dd84fc9fe4859ea005de8d522c6281
fand <fand@gmork.in>
2015-10-06 10:26:14 +0900

    loop毎に一度だけbeat, tickが生成されるように

--- src/js/models/index.js
+++ src/js/models/index.js
@@ -8,33 +8,46 @@ const timerModel = (actions) => {
 
   const interval$ = actions.changeBPM$.map(bpmToInterval);
 
-  const tick$ = (() => {
+  const loop$ = (() => {
     let currentInterval;
     const eventEmitter = new EventEmitter();
-    const ticker = () => {
-      eventEmitter.emit('tick');
-      setTimeout(ticker, currentInterval);
+    const looper = () => {
+      eventEmitter.emit('loop', 1);
+      setTimeout(looper, currentInterval);
     };
 
     interval$.subscribe((interval) => {
       currentInterval = interval;
     });
-    ticker();
+    looper();
 
-    return Rx.Observable.fromEvent(eventEmitter, 'tick').startWith(1);
+    return Rx.Observable.fromEvent(eventEmitter, 'loop').startWith(1);
   })();
 
-  const tickAndInterval$ = Rx.Observable.combineLatest(tick$, interval$);
+  const intervalForLoop$ = loop$.withLatestFrom(
+    interval$,
+    (loop, interval) => (interval)
+  );
+  // const loopWithLatestInterval$ = loop$.withLatestFrom(interval$);
 
-  // tick毎に4つ打ちを生成
-  const beat$ = tickAndInterval$.flatMap(([tick, interval]) =>
+  // loopWithLatestInterval$.subscribe(b => console.log('loopwithint'));
+  // loop$.subscribe(() => console.log('loop'));
+
+  // loop毎に4つ打ちを生成
+  const beat$ = intervalForLoop$.flatMap((interval) =>
     Rx.Observable.interval(interval / 4).take(4)
+  );
+
+  // 64分音符
+  const tick$ = intervalForLoop$.flatMap((interval) =>
+    Rx.Observable.interval(interval / 64).take(4)
   );
 
   return {
     interval$,
-    tick$,
+    loop$,
     beat$,
+    tick$,
   };
 
 };
@@ -42,8 +55,7 @@ const timerModel = (actions) => {
 export default function models (actions) {
 
   const timerModel$ = timerModel(actions);
-  timerModel$.beat$.subscribe(() => console.log('yoD', Date.now()));
-
+  timerModel$.beat$.subscribe(() => console.log('>>>>, ', Date.now()));
   return Cycle.Rx.Observable.combineLatest(
     actions.changeBPM$.startWith(144),
     (bpm) => {
