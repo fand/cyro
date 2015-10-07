a690016c3eb18f193b9052aa138746d30f4a4170
fand <fand@gmork.in>
2015-10-08 01:21:37 +0900

    kickなる

--- src/js/views/index.js
+++ src/js/views/index.js
@@ -2,12 +2,14 @@
 
 import { hJSX } from '@cycle/dom';
 import bpmSlider from './bpmSlider';
+import speaker from './speaker';
 import cyro from './cyro';
 
 export default function views (state$) {
   return state$.map((state) =>
     <div>
       {cyro(state.cyro)}
+      {speaker(state.cyro)}
       {bpmSlider(state.bpm)}
     </div>
   );
---
+++ src/js/views/speaker.js
@@ -0,0 +1,82 @@
+/** @jsx hJSX */
+
+const ctx = new AudioContext();
+
+import { EventEmitter } from 'events';
+class Sample extends EventEmitter {
+
+  constructor (url) {
+    super();
+
+    this.on('sampleLoadSucceeded', (buffer) => {
+      this.buffer = buffer;
+    });
+
+    this.loadSample(url);
+  }
+
+  play (time = 0) {
+    if (this.node) { this.node.stop(0); }
+    this.node = ctx.createBufferSource();
+    this.node.buffer = this.buffer;
+    this.node.connect(ctx.destination);
+    this.node.start(time);
+  }
+
+  loadSample (url) {
+    this.basename = url.split('/').pop();
+
+    var req = new XMLHttpRequest();
+    req.open('GET', url, true);
+    req.responseType = 'arraybuffer';
+
+    req.onload = () => {
+      if (!req.response) {
+        this.emit('sampleLoadFailed', new Error('no response'));
+      }
+      ctx.decodeAudioData(req.response, (buffer) => {
+        this.emit('sampleLoadSucceeded', buffer);
+      }, (err) => {
+        this.emit('sampleLoadFailed', err);
+      });
+    };
+
+    req.send();
+  }
+
+}
+
+const kick = new Sample('./wav/kick.wav');
+
+const state = {
+  offsets   : [],
+  startTime : performance.now(),
+  interval  : 1000,
+  played    : [],
+};
+
+const play = (timestamp) => {
+  const t = timestamp - state.startTime;
+
+  for (let i = 0; i < state.offsets.length; i++) {
+    if (state.played[i]) { continue; }
+    const offset = state.offsets[i] / 64 * state.interval;
+    const diff = t - offset;
+
+    if (0 < diff && diff < 60) {
+      kick.play(diff);
+      state.played[i] = true;
+    }
+  }
+
+  requestAnimationFrame(play);
+
+};
+requestAnimationFrame(play);
+
+export default function speaker ({ interval, startTime, notes }) {
+  state.interval  = interval;
+  state.startTime = startTime;
+  state.offsets   = notes;
+  state.played    = notes.map(() => false);
+}
