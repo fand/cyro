a0878f30f2d7bfd6c568df89a8f13a992b70f4d6
fand <fand@gmork.in>
2015-10-08 17:11:47 +0900

    modelとcyroModelを分割

---
+++ src/js/models/cyroModel.js
@@ -0,0 +1,53 @@
+import { Rx } from '@cycle/core';
+import { EventEmitter } from 'events';
+
+const bpmToInterval = bpm => (60000 / bpm) * 4;
+
+const cyroModel = (actions) => {
+
+  const interval$ = actions.changeBPM$.map(bpmToInterval);
+
+  const loop$ = (() => {
+    let currentInterval;
+    const eventEmitter = new EventEmitter();
+    const looper = () => {
+      eventEmitter.emit('loop', 1);
+      setTimeout(looper, currentInterval);
+    };
+
+    interval$.subscribe((interval) => {
+      currentInterval = interval;
+    });
+    looper();
+
+    return Rx.Observable.fromEvent(eventEmitter, 'loop').startWith(1);
+  })();
+
+  const timestamp$ = loop$.map(() => performance.now());
+
+  const intervalAndTimestamp$ = Rx.Observable.combineLatest(
+    interval$,
+    timestamp$,
+  );
+
+  const note$ = actions.addNote$
+    .withLatestFrom(intervalAndTimestamp$, (e, [interval, timestamp]) => {
+      return ((performance.now() - timestamp) / interval * 64) | 0;
+    }).startWith([0, 16, 32, 48]);
+
+  const notes$ = note$.scan((prev, note) => prev.concat(note));
+
+  const notesAndLoop$ = Rx.Observable.combineLatest(notes$, loop$);
+
+  const stateForLoop$ = notesAndLoop$.withLatestFrom(
+    intervalAndTimestamp$,
+    ([notes], [interval, startTime]) => ({
+      notes, interval, startTime,
+    })
+  );
+
+  stateForLoop$.subscribe(s => console.log(s.notes.length));
+  return stateForLoop$;
+};
+
+export default cyroModel;
--- src/js/models/index.js
+++ src/js/models/index.js
@@ -1,61 +1,11 @@
-import Cycle, {Rx} from '@cycle/core';
-import { EventEmitter } from 'events';
-
-
-const bpmToInterval = bpm => (60000 / bpm) * 4;
-
-const cyroModel = (actions) => {
-
-  const interval$ = actions.changeBPM$.map(bpmToInterval);
-
-  const loop$ = (() => {
-    let currentInterval;
-    const eventEmitter = new EventEmitter();
-    const looper = () => {
-      eventEmitter.emit('loop', 1);
-      setTimeout(looper, currentInterval);
-    };
-
-    interval$.subscribe((interval) => {
-      currentInterval = interval;
-    });
-    looper();
-
-    return Rx.Observable.fromEvent(eventEmitter, 'loop').startWith(1);
-  })();
-
-  const timestamp$ = loop$.map(() => performance.now());
-
-  const intervalAndTimestamp$ = Rx.Observable.combineLatest(
-    interval$,
-    timestamp$,
-  );
-
-  const note$ = actions.addNote$
-    .withLatestFrom(intervalAndTimestamp$, (e, [interval, timestamp]) => {
-      return ((performance.now() - timestamp) / interval * 64) | 0;
-    }).startWith([0, 16, 32, 48]);
-
-  const notes$ = note$.scan((prev, note) => prev.concat(note));
-
-  const notesAndLoop$ = Rx.Observable.combineLatest(notes$, loop$);
-
-  const stateForLoop$ = notesAndLoop$.withLatestFrom(
-    intervalAndTimestamp$,
-    ([notes], [interval, startTime]) => ({
-      notes, interval, startTime,
-    })
-  );
-
-  stateForLoop$.subscribe(s => console.log(s.notes.length));
-  return stateForLoop$;
-};
+import { Rx } from '@cycle/core';
+import cyroModel from './cyroModel';
 
 export default function models (actions) {
 
   const cyroState$ = cyroModel(actions);
 
-  return Cycle.Rx.Observable.combineLatest(
+  return Rx.Observable.combineLatest(
     actions.changeBPM$.startWith(144),
     cyroState$,
     (bpm, cyro) => {
