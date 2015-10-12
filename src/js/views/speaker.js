7c1ca3855fbaf39aab692407670fc0d7c8fbbd01
fand <fand@gmork.in>
2015-10-13 03:46:59 +0900

    trim keys string

--- src/js/views/speaker.js
+++ src/js/views/speaker.js
@@ -5,7 +5,8 @@ const keys = `
   Q W E R T Y U I O P
   A S D F G H J K L
   Z X C V B N M
-`.split(/\s+/);
+`.trim().split(/\s*/);
+console.log(keys);
 const sounds = {};
 keys.forEach((key) => {
   sounds[key] = new Sample(`./wav/${key}.wav`);
